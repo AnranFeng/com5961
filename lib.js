3efee1258a119d8e599d766f73ae51215049c121
Bernard Suen <bsysin@gmail.com>
2017-09-07 08:08:17 +0800

    lib.js updated

--- lib.js
+++
@@ -1,7 +0,0 @@
-var listItems = document.getElementById("navlist");
-
-listItems[0].getElementsByTagName("li").addEventListener("mouseover", myFunction);
-
-function myFunction() {
-    alert ("Hello World!");
-}
